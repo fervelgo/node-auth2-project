1b69c883e2d8a12f8b3d573b0c8c60ddad534ee8
Fernando Velasco <fervelgo@gmail.com>
2021-09-14 23:31:31 +0100

    restircted middleware added

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,6 +1,28 @@
+const { default: jwtDecode } = require("jwt-decode");
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const jwt = require('jsonwebtoken');
 
 const restricted = (req, res, next) => {
+  const token = req.headers.authorization
+  if(!token) {
+    return next({
+      status:401, message: 'Token required'
+    })
+  }
+  
+    jwt.verify(
+      token,
+      JWT_SECRET,
+      (err, decoded) => {
+        if(err) return next({
+          status:401, message: 'Token invalid', realErrorMessage: err.message,
+        })
+        req.decodedJwt = decoded
+        req.token = token
+        next()
+      }
+    )
+
   /*
     If the user does not provide a token in the Authorization header:
     status 401
